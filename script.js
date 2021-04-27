e48f29571539c33edf766424ea50370bc57a4691
vagfou <foufoulas.evangelos@protonmail.com>
2021-04-27 16:02:33 +0300

    search implemented

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ const listItems = []
 
 getData()
 
+filter.addEventListener('input', (e) => filterData(e.target.value))
+
 async function getData() {
     const res = await fetch('https://randomuser.me/api?results=50')
 
     const {results} = await res.json()
 
     //Clear results
-    results.innerHTML = ''
+    result.innerHTML = ''
     results.forEach(user => {
         const li = document.createElement('li')
 
@@ -23,5 +25,15 @@ async function getData() {
             </div>
         `
         result.appendChild(li)
+    })
+}
+
+function filterData(searchTerm) {
+    listItems.forEach(item => {
+        if(item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+            item.classList.remove('hide')
+        } else {
+            item.classList.add('hide')
+        }
     })
 }
